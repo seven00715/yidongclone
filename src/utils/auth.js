83c42cfd4ef5bd0f401e0c320827862e69a20a29
seven00715 <1027369812@qq.com>
2020-01-29 15:05:44 +0800

    用户信息操作工具类

---
+++ src/utils/auth.js
@@ -0,0 +1,16 @@
+const USE_TOKEN = 'hm-m-91-token'
+
+// 定义三个方法进行token的存,取,删
+
+// 读取用户信息
+export function getToken () {
+  return JSON.parse(localStorage.getItem(USE_TOKEN) || '{}') // 短路表达式  USE_TOKEN没有默认为空
+}
+// 设置用户信息
+export function setToken (use) {
+  return localStorage.setItem(USE_TOKEN, JSON.stringify(use))
+}
+// 移除用户信息
+export function remove () {
+  return localStorage.removeItem(USE_TOKEN)
+}
