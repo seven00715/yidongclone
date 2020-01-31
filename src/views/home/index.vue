e44a16a8742c6ee1ca6a17043ce87282224a983e
seven00715 <1027369812@qq.com>
2020-01-31 12:42:43 +0800

    使用tab组件

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,13 +1,20 @@
 <template>
   <div class="container">
-    <van-tabs>
+    <van-tabs swipeable v-model="activeIndex">
       <van-tab v-for="index in 8" :key="index" :title="'标签 ' + index">
-        <van-pull-refresh v-model="refreshing" @refresh="onRefresh">
+        <!--  这里坐了一个滚动条目的是为了后面的 阅读记忆 留下伏笔 -->
+        <!-- 阅读记忆 看文章看到一半滑到中部  跳转到了别的页面 当你回来时 文章还在你看的位置  -->
+        <div class="scroll-wrapper">
+          <van-pull-refresh v-model="refreshing" @refresh="onRefresh">
           <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
-            <van-cell v-for="item in list" :key="item" :title="item" />
+            <van-cell v-for="item in 20" :key="item" :title="index" ></van-cell>
           </van-list>
         </van-pull-refresh>
+        </div>
       </van-tab>
+      <span class="bar_btn">
+        <van-icon name="wap-nav"></van-icon>
+      </span>
     </van-tabs>
   </div>
 </template>
@@ -17,6 +24,7 @@ export default {
   name: 'home',
   data () {
     return {
+      activeIndex: 1, // 默认启动第0个标签
       list: [],
       loading: false,
       finished: false,
@@ -55,7 +63,59 @@ export default {
 </script>
 
 <style lang="less" scoped>
-.container {
-  font-size: 16px;
+.van-tabs {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  /deep/ .van-tabs__wrap {
+    height: 36px;
+    padding-right: 36px;
+    .van-tab {
+      line-height: 36px;
+    }
+    .van-tabs__line {
+      background-color: #3296fa;
+      height: 2px;
+    }
+  }
+  /deep/ .van-tabs__content{
+    flex: 1;
+    overflow: hidden;
+  }
+  /deep/ .van-tab__pane{
+    height: 100%;
+    .scroll-wrapper{
+      height: 100%;
+      overflow-y: auto;
+    }
+  }
+}
+.bar_btn {
+  width: 36px;
+  height: 35px;
+  position: absolute;
+  top: 0;
+  right: 0;
+  &::before {
+    content: "";
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    z-index: 999;
+    box-shadow: 0 0 10px #999;
+    transform: scale(1, 0.6);
+  }
+  .van-icon-wap-nav {
+    width: 100%;
+    height: 100%;
+    background: #fff;
+    text-align: center;
+    line-height: 35px;
+    position: relative;
+    z-index: 1000;
+    &::before {
+      font-size: 20px;
+    }
+  }
 }
 </style>
